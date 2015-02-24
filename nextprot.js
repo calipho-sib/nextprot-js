1a8747a26fd89405201f9ef4fae8b6938baac5bf
Daniel Teixeira <ddtxra@gmail.com>
2015-02-24 23:47:52 +0100

    Update nextprot.js

--- nextprot.js
+++ nextprot.js
@@ -1,1 +1,18 @@
+var nextprotApiUrl = "https://api.nextprot.org/entry/";
 
+var nextprot = {
+
+  getProteinSequence: function (entry, cb){
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = nextprotApiUrl + entry + "/protein-sequence.json";
+
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        if(cb) cb(JSON.parse(xmlhttp.responseText));
+      }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+}
