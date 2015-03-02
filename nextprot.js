b268243babee9e6fb078f953792cd5a8404ca03e
Daniel Teixeira <ddtxra@gmail.com>
2015-03-02 16:46:10 +0100

    Update nextprot.js

--- nextprot.js
+++ nextprot.js
@@ -1,11 +1,30 @@
-var nextprotApiUrl = "https://api.nextprot.org/entry/";
 
-var nextprot = {
+var nx = new nextprot();
 
-  getProteinSequence: function (entry, cb){
+function nextprot() {
+
+  this.nextprotApiUrl = "https://api.nextprot.org/entry/";
+
+  //Util methods
+
+  this.getURLParameter = function (name){
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  };
+
+  //Gets the entry set in the parameter
+  this.getEntryName = function(){
+    return this.getURLParameter("nxentry") || 'NX_P01308'; //By default returns the insulin
+  }
+
+
+
+  this.callURL = function (context, cb){
 
     var xmlhttp = new XMLHttpRequest();
-    var url = nextprotApiUrl + entry + "/protein-sequence.json";
+    var url = this.nextprotApiUrl + this.getEntryName() + "/" + context + ".json";
 
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -14,5 +33,26 @@ var nextprot = {
     }
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+
   }
+
+  this.getProteinSequence = function(cb) {
+    return this.callURL("protein-sequence", function (data){
+      if(cb) cb(data.entry.isoforms[0].sequence);
+    });
+  };
+
+  this.getProteinOverview = function(cb) {
+    return this.callURL("overview", function (data){
+      if(cb) cb(data.entry.overview);
+    });
+  };
+
+
+  this.getSecondaryStructure = function(cb) {
+    return this.callURL("secondary-structure", function (data){
+      if(cb) cb(data.entry.annotations);
+    });
+  };
+
 }
