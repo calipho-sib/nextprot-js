b68dea6b33cd60d62385fc70e34e810bfe53919e
Mathieu Schaeffer <the.arthek@gmail.com>
2015-04-16 11:52:14 +0200

    add method to get json from sparql queries

--- src/nextprot.js
+++ src/nextprot.js
@@ -11,7 +11,37 @@
 
     (function () {
 
+        //?default-graph-uri=&named-graph-uri=&output=json
+
         var nextprotApiUrl = "https://api.nextprot.org/entry/";
+        var sparqlEndpoint = "http://alpha-api.nextprot.org/sparql";
+        var sparqlFormat = "?output=json";
+        var sparqlPrefixes = "PREFIX :<http://nextprot.org/rdf#> "+
+            "PREFIX annotation:<http://nextprot.org/rdf/annotation/> "+
+            "PREFIX context:<http://nextprot.org/rdf/context/> "+
+            "PREFIX cv:<http://nextprot.org/rdf/terminology/> "+
+            "PREFIX db:<http://nextprot.org/rdf/db/> "+
+            "PREFIX dc:<http://purl.org/dc/elements/1.1/> "+
+            "PREFIX dcterms:<http://purl.org/dc/terms/> "+
+            "PREFIX entry:<http://nextprot.org/rdf/entry/> "+
+            "PREFIX evidence:<http://nextprot.org/rdf/evidence/> "+
+            "PREFIX foaf:<http://xmlns.com/foaf/0.1/> "+
+            "PREFIX gene:<http://nextprot.org/rdf/gene/> "+
+            "PREFIX identifier:<http://nextprot.org/rdf/identifier/> "+
+            "PREFIX isoform:<http://nextprot.org/rdf/isoform/> "+
+            "PREFIX mo:<http://purl.org/ontology/mo/> "+
+            "PREFIX ov:<http://open.vocab.org/terms/> "+
+            "PREFIX owl:<http://www.w3.org/2002/07/owl#> "+
+            "PREFIX publication:<http://nextprot.org/rdf/publication/> "+
+            "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+            "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> "+
+            "PREFIX sim:<http://purl.org/ontology/similarity/> "+
+            "PREFIX source:<http://nextprot.org/rdf/source/> "+
+            "PREFIX term:<http://nextprot.org/rdf/terminology/> "+
+            "PREFIX xref:<http://nextprot.org/rdf/xref/> "+
+            "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> ";
+
+
 
         var NextprotClient = function () {
         };
@@ -59,6 +89,14 @@
             });
         };
 
+        NextprotClient.prototype.executeSparql = function(sparql) {
+
+            var sparqlQuery = sparqlPrefixes+sparql;
+            var url = sparqlEndpoint+sparqlFormat+"&query="+encodeURIComponent(sparqlQuery);
+            return Promise.resolve($.getJSON(url)).then(function (data){
+                return data;
+            });
+        };
 
         NextprotClient.prototype.getProteinOverview = function() {
             return _callURL(this.getEntryName(), "overview").then(function (data){
