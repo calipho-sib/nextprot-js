d66db39989498f733c601b5d20bb220f8561ac72
Mathieu Schaeffer <the.arthek@gmail.com>
2015-04-21 16:49:10 +0200

    file transfer

---
+++ dist/nextprot.js
@@ -0,0 +1,149 @@
+/**
+ * A neXtProt js client
+ */
+( function (root) {
+    //
+    'use strict';
+    if (root.Nextprot === undefined) {
+        root.Nextprot = {};
+    }
+
+
+    (function () {
+
+        //?default-graph-uri=&named-graph-uri=&output=json
+
+        var nextprotApiUrl = "https://api.nextprot.org/entry/";
+        var sparqlEndpoint = "http://alpha-api.nextprot.org/sparql";
+        var sparqlFormat = "?output=json";
+        var sparqlPrefixes = "PREFIX :<http://nextprot.org/rdf#> "+
+            "PREFIX annotation:<http://nextprot.org/rdf/annotation/> "+
+            "PREFIX context:<http://nextprot.org/rdf/context/> "+
+            "PREFIX cv:<http://nextprot.org/rdf/terminology/> "+
+            "PREFIX db:<http://nextprot.org/rdf/db/> "+
+            "PREFIX dc:<http://purl.org/dc/elements/1.1/> "+
+            "PREFIX dcterms:<http://purl.org/dc/terms/> "+
+            "PREFIX entry:<http://nextprot.org/rdf/entry/> "+
+            "PREFIX evidence:<http://nextprot.org/rdf/evidence/> "+
+            "PREFIX foaf:<http://xmlns.com/foaf/0.1/> "+
+            "PREFIX gene:<http://nextprot.org/rdf/gene/> "+
+            "PREFIX identifier:<http://nextprot.org/rdf/identifier/> "+
+            "PREFIX isoform:<http://nextprot.org/rdf/isoform/> "+
+            "PREFIX mo:<http://purl.org/ontology/mo/> "+
+            "PREFIX ov:<http://open.vocab.org/terms/> "+
+            "PREFIX owl:<http://www.w3.org/2002/07/owl#> "+
+            "PREFIX publication:<http://nextprot.org/rdf/publication/> "+
+            "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+            "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> "+
+            "PREFIX sim:<http://purl.org/ontology/similarity/> "+
+            "PREFIX source:<http://nextprot.org/rdf/source/> "+
+            "PREFIX term:<http://nextprot.org/rdf/terminology/> "+
+            "PREFIX xref:<http://nextprot.org/rdf/xref/> "+
+            "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> ";
+
+
+
+        var NextprotClient = function () {
+        };
+
+        //Util methods
+        var _getURLParameter = function (name){
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        };
+
+        //Gets the entry set in the parameter
+        NextprotClient.prototype.getEntryName = function(){
+            return _getURLParameter("nxentry") || 'NX_P01308'; //By default returns the insulin
+        };
+
+        //private method, convention use an underscore
+        var _callURL = function (entryName, context){
+
+            var me = this;
+
+            return new Promise(function(resolve, reject) {
+
+                var req = new XMLHttpRequest();
+                var url = nextprotApiUrl + entryName + "/" + context + ".json";
+                req.open("GET", url);
+
+                req.onload = function() {
+                    // This is called even on errors so check the status
+                    if (req.status == 200) {
+                        resolve(JSON.parse(req.responseText));
+                    }else {
+                        reject(Error(req.statusText));
+                    }
+                };
+
+                // Handle network errors
+                req.onerror = function() {
+                    reject(Error("Network Error"));
+                };
+
+                // Make the request
+                req.send();
+            });
+        };
+
+        NextprotClient.prototype.executeSparql = function(sparql) {
+
+            var sparqlQuery = sparqlPrefixes+sparql;
+            var url = sparqlEndpoint+sparqlFormat+"&query="+encodeURIComponent(sparqlQuery);
+            return Promise.resolve($.getJSON(url)).then(function (data){
+                return data;
+            });
+        };
+
+        NextprotClient.prototype.getProteinOverview = function() {
+            return _callURL(this.getEntryName(), "overview").then(function (data){
+                return data.entry.overview;
+            });
+        };
+
+        NextprotClient.prototype.getProteinSequence = function() {
+            return _callURL(this.getEntryName(), "isoform").then(function (data){
+                return data.entry.isoforms;
+            });
+        };
+
+        NextprotClient.prototype.getSecondaryStructure = function() {
+            return _callURL(this.getEntryName(), "secondary-structure").then(function (data){
+                return data.entry.annotations;
+            });
+        };
+
+        NextprotClient.prototype.getMatureProtein = function() {
+            return _callURL(this.getEntryName(), "mature-protein").then(function (data){
+                return data.entry.annotations;
+            });
+        };
+
+        NextprotClient.prototype.getPeptide = function(entry) {
+            return _callURL(entry || this.getEntryName(), "peptide").then(function (data){
+                return data.entry.peptideMappings;
+            });
+        };
+
+        NextprotClient.prototype.getSrmPeptide = function(entry) {
+            return _callURL(entry || this.getEntryName(), "srm-peptide").then(function (data){
+                return data.entry.srmPeptideMappings;
+            });
+        };
+
+
+        //node.js compatibility
+        if (typeof exports !== 'undefined') {
+            exports.Client = NextprotClient;
+        }
+
+
+        root.Nextprot.Client = NextprotClient;
+
+    }());
+
+
+}(this));
